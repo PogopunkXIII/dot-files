54c78e4aeed37c4a1ad763b8ad7dfe0ce20697ff
Jesse Mailhot <jesse.mailhot@pega.com>
2024-12-03 09:47:16 -0500

    added a few more language servers

--- nvim/init.lua
+++ nvim/init.lua
@@ -543,7 +543,10 @@ require("lazy").setup({
 				-- But for many setups, the LSP (`ts_ls`) will work just fine
 				ts_ls = {},
 				--
-
+				cssls = {},
+				css_variables = {},
+				cssmodules_ls = {},
+				ast_grep = {},
 				lua_ls = {
 					-- cmd = {...},
 					-- filetypes = { ...},
