97bf6eb2fce9f86baaf0639945bcc3fdd7e5fd34
pogopunkxiii <mailhot.jesse@gmail.com>
2025-06-24 07:36:02 -0400

    forgot to move vimtext over to the new organization

---
+++ .config/nvim/lua/custom/plugins/vim-test.lua
@@ -0,0 +1,13 @@
+return {
+	"vim-test/vim-test",
+	dependencies = {
+		"preservim/vimux",
+	},
+	vim.keymap.set("n", "<leader>t", "<cmd>TestNearest<CR>", { desc = "Run test nearest the cursor" }),
+	vim.keymap.set("n", "<leader>T", "<cmd>TestFile<CR>", { desc = "Runs the entire test file" }),
+	vim.keymap.set("n", "<leader>a", "<cmd>TestSuite<CR>", { desc = "Runs entire test suite" }),
+	vim.keymap.set("n", "<leader>l", "<cmd>TestLast<CR>", { desc = "Runs last test ran" }),
+	vim.keymap.set("n", "<leader>g", "<cmd>TestVisit<CR>", { desc = "navigate to the last test file that was run" }),
+
+	vim.cmd("let test#strategy = 'vimux'"),
+}
