5c4934dad9a51ee3767fdd2ccaf23a9bdf878c3a
mailj_pega <jesse.mailhot@pega.com>
2025-09-15 11:28:26 -0400

    some small changes

--- .config/nvim/lua/kickstart/plugins/telescope.lua
+++ .config/nvim/lua/kickstart/plugins/telescope.lua
@@ -52,6 +52,11 @@ return {
 			-- [[ Configure Telescope ]]
 			-- See `:help telescope` and `:help telescope.setup()`
 			require("telescope").setup({
+				-- defaults = {
+				-- 	file_ignore_patterns = {
+				-- 		"node_modules",
+				-- 	},
+				-- },
 				-- You can put your default mappings / updates / etc. in here
 				--  All the info you're looking for is in `:help telescope.setup()`
 				--
@@ -76,7 +81,10 @@ return {
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>sf", function()
+				-- file_ignore_patterns = { "/test/", "^test/" }
+				builtin.find_files({ hidden = true })
+			end, { desc = "[S]earch [F]iles" })
 			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
